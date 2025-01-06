8599db6260993983ff38738f28684c5078b4d933
Absterrg0 <parvj5212@gmail.com>
2025-01-06 19:00:10 +0530

    Increased the upload size

--- index.ts
+++ index.ts
@@ -22,7 +22,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ 
   storage: storage,
-  limits: { fileSize: 100 * 1024 * 1024 }
+  limits: { fileSize:5 * 1024 * 1024 * 1024 }
 });
 
 app.use(cors());
